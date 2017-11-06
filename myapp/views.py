27c165158c2a9535ff28aec45fee762eaddab3ef
Harry <hjwp2@cantab.net>
2017-11-06 11:54:59 +0000

    add a database model

--- myapp/views.py
+++ myapp/views.py
@@ -2,8 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import BlogPost
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'posts': list(BlogPost.objects.all())})
 
